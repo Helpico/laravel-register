97b6ff8a9be8a08159b1690696dfaf576b4456f3
Maksym Dudyk <novamova@ukr.net>
2021-04-05 03:02:12 +0300

    In the middle of queue processing 2

---
+++ app/Jobs/SendMessage.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class SendMessage implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    // class instance by Job 
+    protected $message; 
+    
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __constructor($data)
+    {
+        $this->message = $data;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        info($this->message);
+    }
+}
